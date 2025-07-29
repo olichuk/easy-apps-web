3b86420c9c0feeb79968bc023aaef61baef093ed
dmittruc <dimon22012201@gmail.com>
2025-07-29 22:48:10 +0300

    fix: add default route login

--- src/components/AppNavigation/index.tsx
+++ src/components/AppNavigation/index.tsx
@@ -1,29 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+/** @format */
+
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import SignInPage from "../../pages/SignInPage/index";
 import React from "react";
-import ProtectedRoute from "../ProtectedRoute";
 import SignUpPage from "../../pages/SignUpPage/SignUp";
 
 const AppNavigation = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/login"
-          element={
-            <ProtectedRoute>
-              <SignInPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/registration"
-          element={
-            <ProtectedRoute>
-              <SignUpPage />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/login" element={<SignInPage />} />
+        <Route path="/registration" element={<SignUpPage />} />
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
   );
