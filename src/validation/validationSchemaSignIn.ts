949f30b33054a1efdba9ec59707b20521d500bd9
dmittruc <dimon22012201@gmail.com>
2025-08-01 15:16:36 +0300

    fix: fix validation rules

--- src/validation/validationSchemaSignIn.ts
+++ src/validation/validationSchemaSignIn.ts
@@ -1,9 +1,12 @@
+/** @format */
+
 import * as Yup from "yup";
 
 const validationSchema = Yup.object().shape({
   password: Yup.string()
-    .required("Please enter your password")
     .min(8, "Password must be at least 8 characters")
+    .max(64, "Password cannot exceed 64 characters")
+    .required("Please enter your password")
     .matches(/[A-Za-z]/, "Password must include at least one letter"),
 
   email: Yup.string()
--- src/validation/validationSchemaSignUp.ts
+++ src/validation/validationSchemaSignUp.ts
@@ -6,12 +6,14 @@ const validationSchemaSignUp = Yup.object().shape({
   email: Yup.string().email().required("Email is required").label("Email"),
   name: Yup.string().min(2).required("Name is required").label("Name"),
   password: Yup.string()
-    .min(6)
-    .required("Password is required")
+    .min(8, "Password must be at least 8 characters")
+    .max(64, "Password cannot exceed 64 characters")
+    .required("Please enter your password")
+    .matches(/[A-Za-z]/, "Password must include at least one letter")
     .label("Password"),
   repeatPassword: Yup.string()
     .oneOf([Yup.ref("password")], "Passwords must match")
-    .required("Repeat password is required")
+    .required("Please enter your password again")
     .label("Repeat password"),
   avatar: Yup.string(),
 });
